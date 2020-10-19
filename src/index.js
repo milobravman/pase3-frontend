d375e41a5549795b02c1e06528e6f1dbf23a3c3a
github username <github email address>
2020-10-19 15:12:24 -0400

    fetching parts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-document.addEventListener("DOMContentLoaded", ()=> {
+
+const URL = "http://localhost:3000"
+
+function fetchMonster() {
     fetch("http://localhost:3000/monsters")
     .then(res => res.json())
     .then(console.log)
+}
+
+function fetchParts() {
+    fetch("http://localhost:3000/parts")
+    .then(res => res.json())
+    .then(res => {
+        res.forEach(part => {
+            renderPart(part)
+        })
+    })
+}
+
+//fetch("http://localhost:3000/parts")
+//.then(res => res.json())
+//.then(partsData => renderParts(partsData))
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    fetchMonster()
+    fetchParts()
 })
+
+function renderPart(data) {
+    partsDiv = document.querySelector("#partsList")
+    
+    let partImage = document.createElement("img")
+    partImage.src = data.image
+
+    partsDiv.appendChild(partImage)
+
+    
+    
+}
